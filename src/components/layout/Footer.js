7b4a37a9e9b918b4a7e65a1920be65e93c0038b6
publdaze <publdaze@naver.com>
2024-08-23 21:02:36 +0900

    feat: 푸터 컴포넌트 작성

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+import Component from "../Component.js";
+
+export default class Footer extends Component {
+  template() {
+    return `
+      <div class="footer-content">
+        <p class="copyright">© 2024 publdaze</p>
+      </div>
+    `;
+  }
+}
